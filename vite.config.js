060164d8f2d787e18525804fb9c6408dc6064f91
Ilan Lallemand <ilan@MacBook-Pro-de-Ilan.local>
2023-09-21 16:35:52 +0200

    change vite setting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Ilan916/news-homepage',
+  base: '/news-homepage/',
   plugins: [react()],
 })
